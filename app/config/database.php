619b81237d45a55118631958e4adecb2e90fea52
WritingPanda <omar.quimbaya4@gmail.com>
2014-03-26 11:30:46 -0500

    Database preparation.
    Getting started on databases using Laravel.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'blog_db',
+			'username'  => 'omar',
+			'password'  => 'geekdom1100',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
