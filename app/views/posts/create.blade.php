c714e38364f2a27871ef395da6d92a5899b9bab4
WritingPanda <omar.quimbaya4@gmail.com>
2014-03-27 13:57:13 -0500

    Testing out Laravel building HTML
    Using blade and Laravel to build form

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -5,17 +5,17 @@
 <hr>
 <div class='blog-post'></div>
 <form action="{{{ action('PostsController@store') }}}" role='form' class='form-horizontal' method='POST'>
-	<div class='form-group'>
+	<div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
 		<label for='title' class='control-label col-lg-2'>Title</label>
 		<div class='col-lg-8'>
     		<input class='form-control' type='text' id='title' name='title' autofocus='autofocus' value="{{{ Input::old('title') }}}">
     		{{ $errors->has('title') ? $errors->first('title', "<span class='help-block'>:message</span>") : '' }}
     	</div>
 	</div>
-	<div class='form-group'>
+	<div class="form-group {{ $errors->has('body') ? 'has-error' : '' }}">
 		<label for="body" class='col-lg-2 control-label'>Body</label>
-		<div class='col-sm-8'>
-			<textarea class='form-control' name='body' id='body' rows='4'>{{{ Input::old('body') }}}</textarea>
+		<div class='col-lg-8'>
+			<textarea class='form-control' name='body' id='body' rows='5'>{{{ Input::old('body') }}}</textarea>
 			{{ $errors->has('body') ? $errors->first('body', "<span class='help-block'>:message</span>") : '' }}
 		</div>
 	</div>
