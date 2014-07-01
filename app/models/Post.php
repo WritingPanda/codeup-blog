dd725c7d119e7facdd214f2ee3eeadd81c7a94ac
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-07-01 10:28:57 -0500

    Updating blog.dev

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,13 +2,16 @@
 
 class Post extends BaseModel {
 
+	// The db table this model relates to
     protected $table = 'posts';
 
+    // Validation rules for the model properties
     public static $rules = [
 	    'title'      => 'required|max:100',
 	    'body'       => 'required|max:10000'
 	];
 
+	// Method to show relationship
 	public function user()
 	{
 	    return $this->belongsTo('User');
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<link rel="stylesheet" type="text/css" href="/css/readable-bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/site.css">
